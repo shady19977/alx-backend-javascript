97371b6580f4f913cccde3b7dfb5714839875589
shady19977 <126839414+shady19977@users.noreply.github.com>
2024-08-09 16:43:01 +0300

    Add files via upload

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,5 @@
+export const weakMap = WeakMap();
+
+export default function queryAPI(endpoint) {
+
+}
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+export default function updateStudentGradeByCity(StudentList, city, newGrades) {
+  return StudentList.filter((obj) => obj.location === city).map((obj) => {
+    const ObjEdit = obj;
+    let grade = newGrades.filter((GradeObj) => GradeObj.studentId === obj.id);
+    if (grade.length === 0) {
+      grade = 'N/A';
+    } else {
+      grade = grade[0].grade;
+    }
+    ObjEdit.grade = grade;
+    return ObjEdit;
+  });
+}
